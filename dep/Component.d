98363dc2e95fac66820ef12ac84002b49814811f
Samuel Andrade de Matos <43278142+SamAndMatos@users.noreply.github.com>
2023-09-23 11:53:53 -0300

    Removing bin and dep

--- dep/Component.d
+++
@@ -1,2 +0,0 @@
-bin/Component.o: src/Component.cpp include/Component.h
-include/Component.h:
--- dep/Rect.d
+++
@@ -1,3 +0,0 @@
-bin/Rect.o: src/Rect.cpp include/Rect.h include/Vec2.h
-include/Rect.h:
-include/Vec2.h:
